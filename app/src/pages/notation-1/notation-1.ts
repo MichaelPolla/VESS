a603e3984c22daec3247a4cfdd95a3856dccc27d
ciccius <salvatore.cicciu@gmail.com>
2016-11-21 23:12:15 +0100

    add features view of step 1 for notation and beginning of features for of step 2

---
+++ app/src/pages/notation-1/notation-1.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+
+import { Notation2Page } from '../notation-2/notation-2';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the Notation2 page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-notation-1',
+  templateUrl: 'notation-1.html'
+})
+export class Notation1Page {
+  //declaration of field
+  itens: Array<{title: string, checked: Boolean, imgSrc:String, code:number}>;
+  code:number;
+  constructor(public navCtrl: NavController) {
+    //construction of the list
+    this.itens = [
+      { title: 'Pas de motte fermée', checked: false, imgSrc: './assets/icon/motte.png', code:1},
+      { title: 'Présence possible de mottes fermées', checked: true, imgSrc: './assets/icon/motte.png',code:2 },
+      { title: 'Présence majoritaires de mottes fermées', checked:false, imgSrc: './assets/icon/motte.png',code:3}
+    ];
+  }
+
+  //Methods
+  validationStep(p){
+      this.navCtrl.push(Notation2Page, {
+      code: this.code,
+    })
+       .catch(()=> console.log('should I stay or should I go now'))
+  }
+
+  updateCheckedBox(position, item){
+    //declar var
+    let cnt: number = 0;
+
+    for(let item of this.itens) {
+      //if position selected
+      if(position==cnt){
+        item.checked=true;
+        this.code=item.code;
+      }else{
+        item.checked=false;
+      }
+      cnt++;
+    }
+  }
+
+}
---
+++ app/src/pages/notation-2/notation-2.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the Notation2 page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-notation-2',
+  templateUrl: 'notation-2.html'
+})
+export class Notation2Page {
+
+  constructor(public navCtrl: NavController) {}
+
+  ionViewDidLoad() {
+    console.log('Hello Notation2Page Page');
+  }
+
+}
