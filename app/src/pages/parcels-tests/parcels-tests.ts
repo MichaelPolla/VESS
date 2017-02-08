30533c07d6ebd6e24ecd5ec9f7d56bc68c36c56d
Michaël Polla <michael@polla.net>
2017-02-08 22:46:57 +0100

    Add parcels list. Select an item update the page.

--- app/src/pages/parcels-tests/parcels-tests.ts
+++ app/src/pages/parcels-tests/parcels-tests.ts
@@ -2,12 +2,9 @@ import { Component } from '@angular/core';
 import { NavController, AlertController } from 'ionic-angular';
 
 /*
-  Generated class for the ParcelsTests page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-
-  Docs : https://www.joshmorony.com/an-introduction-to-lists-in-ionic-2/
+  Docs :
+   - Introduction to lists : https://www.joshmorony.com/an-introduction-to-lists-in-ionic-2/
 */
 @Component({
   selector: 'page-parcels-tests',
@@ -15,89 +12,75 @@ import { NavController, AlertController } from 'ionic-angular';
 })
 export class ParcelsTestsPage {
 
-  notes: any = [];
-
-
+  pageTitle = "Parcels";
+  items: any = [];
 
   constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
-      this.notes = [
-            'Bread',
-            'Milk',
-            'Cheese',
-            'Snacks',
-            'Apples',
-            'Bananas',
-            'Peanut Butter',
-            'Chocolate',
-            'Avocada',
-            'Vegemite',
-            'Muffins',
-            'Paper towels'
-        ];
+
   }
 
-  ionViewDidLoad() {
-    console.log('Hello ParcelsTestsPage Page');
-  }
+  addItem() {
 
-  addNote(){
- 
-        let prompt = this.alertCtrl.create({
-            title: 'Add Note',
-            inputs: [{
-                name: 'title'
-            }],
-            buttons: [
-                {
-                    text: 'Cancel'
-                },
-                {
-                    text: 'Add',
-                    handler: data => {
-                        this.notes.push(data);
-                    }
-                }
-            ]
-        });
- 
-        prompt.present();
-    }
- 
-    editNote(note){
- 
-        let prompt = this.alertCtrl.create({
-            title: 'Edit Note',
-            inputs: [{
-                name: 'title'
-            }],
-            buttons: [
-                {
-                    text: 'Cancel'
-                },
-                {
-                    text: 'Save',
-                    handler: data => {
-                        let index = this.notes.indexOf(note);
- 
-                        if(index > -1){
-                          this.notes[index] = data;
-                        }
-                    }
-                }
-            ]
-        });
- 
-        prompt.present();       
- 
-    }
- 
-    deleteNote(note){
- 
-        let index = this.notes.indexOf(note);
- 
-        if(index > -1){
-            this.notes.splice(index, 1);
+    let prompt = this.alertCtrl.create({
+      title: 'Add Item',
+      inputs: [{
+        name: 'title'
+      }],
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Add',
+          handler: data => {
+            this.items.push(data);
+          }
         }
+      ]
+    });
+
+    prompt.present();
+  }
+
+  editItem(item) {
+
+    let prompt = this.alertCtrl.create({
+      title: 'Edit Note',
+      inputs: [{
+        name: 'title'
+      }],
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            let index = this.items.indexOf(item);
+
+            if (index > -1) {
+              this.items[index] = data;
+            }
+          }
+        }
+      ]
+    });
+
+    prompt.present();
+
+  }
+
+  deleteItem(item) {
+
+    let index = this.items.indexOf(item);
+
+    if (index > -1) {
+      this.items.splice(index, 1);
     }
+  }
+
+  itemClicked() {
+    this.pageTitle = "Tests";
+  }
 
 }
