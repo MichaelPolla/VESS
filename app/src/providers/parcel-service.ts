bfa1cb9899e7b76ad86594c37c5b2f4fe13a67dd
Michaël Polla <michael@polla.net>
2017-03-28 00:29:01 +0200

    Reorganize code.

--- app/src/providers/parcel-service.ts
+++ app/src/providers/parcel-service.ts
@@ -9,40 +9,40 @@ export class ParcelService {
   constructor(public storage: Storage) { }
 
   /**
+   * Get parcels data.  
+   * Return : a Promise
+   */
+  public getParcels(): Promise<any> {
+    return new Promise((resolve: any, reject: any) => {
+      if (this.data) { // data were previously loaded ; simply return them.
+        resolve(this.data);
+      } else {  // Must load data first.
+        this.load("parcels").then(() => { resolve(this.data) });
+      }
+    });
+  }
+
+    /**
+   * Save data using ionic Storage (key/value pair).  
+   * key : key associated to the value.  
+   * value : the value to save. 
+   */
+  public save(key: string, value: any) {
+    this.storage.set(key, JSON.stringify(value));
+  }
+
+    /**
    * Get data from Storage.  
    * key : the key associated to the desired values.  
    * Return : a Promise (from Storage)
    */
-  load(key: string): Promise<any> {
+  private load(key: string): Promise<any> {
     return this.storage.get(key).then((value) => {
       if (value != null) {
         this.data = JSON.parse(value);
       }
       else {
         console.log("Storage empty.");
-      }
-    });
-  }
-
-  /**
-   * Save data using ionic Storage (key/value pair).  
-   * key : key associated to the value.  
-   * value : the value to save. 
-   */
-  save(key: string, value: any) {
-    this.storage.set(key, JSON.stringify(value));
-  }
-
-  /**
-   * Get parcels data.  
-   * Return : a Promise
-   */
-  getParcels(): Promise<any> {
-    return new Promise((resolve: any, reject: any) => {
-      if (this.data) { // data were previously loaded ; simply return them.
-        resolve(this.data);
-      } else {  // Must load data first.
-        this.load("parcels").then(() => { resolve(this.data) });
       }
     });
   }
