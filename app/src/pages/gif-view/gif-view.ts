59e81dc6d3df32b0b9a2ce1fe91799c2fc6db736
Michaël Polla <michael@polla.net>
2018-08-23 16:09:39 +0200

    VS Code auto-formatting

--- app/src/pages/gif-view/gif-view.ts
+++ app/src/pages/gif-view/gif-view.ts
@@ -1,17 +1,17 @@
-import { Notation1Page } from './../notation-1/notation-1';
-import { Component } from '@angular/core';
-import { NavController, NavParams, Platform } from 'ionic-angular';
+import { Notation1Page } from "./../notation-1/notation-1";
+import { Component } from "@angular/core";
+import { NavController, NavParams, Platform } from "ionic-angular";
 
 // Pages
-import { CameraPage } from '../camera/camera';
-import { DefiningLayerPage } from '../defining-layer/defining-layer';
+import { CameraPage } from "../camera/camera";
+import { DefiningLayerPage } from "../defining-layer/defining-layer";
 // Providers
-import { DataService } from '../../providers/data-service';
-import { TranslateProvider } from '../../providers/translate/translate'
+import { DataService } from "../../providers/data-service";
+import { TranslateProvider } from "../../providers/translate/translate";
 
 @Component({
-  selector: 'page-gif-view',
-  templateUrl: 'gif-view.html'
+  selector: "page-gif-view",
+  templateUrl: "gif-view.html"
 })
 export class GifViewPage {
   stepView: number;
@@ -23,52 +23,50 @@ export class GifViewPage {
     public navCtrl: NavController,
     public navParams: NavParams,
     private platform: Platform,
-    private translate: TranslateProvider) {
-
+    private translate: TranslateProvider
+  ) {
     //test if is the first step or other step
-    if (this.navParams.get('stepView') == null) {
+    if (this.navParams.get("stepView") == null) {
       this.stepView = 0;
-        this.title = translate.get('BLOCK_EXTRACTION');
+      this.title = translate.get("BLOCK_EXTRACTION");
     } else {
-      this.stepView = this.navParams.get('stepView');
-        this.title = translate.get('BLOCK_OPENING');
+      this.stepView = this.navParams.get("stepView");
+      this.title = translate.get("BLOCK_OPENING");
     }
 
     //image in function of step
     switch (this.stepView) {
       case 0:
-        this.imageFile = './assets/gifs/1extraction_bloc.gif';
+        this.imageFile = "./assets/gifs/1extraction_bloc.gif";
         break;
       case 3:
-        this.imageFile = './assets/gifs/2ouverture_bloc.gif';
+        this.imageFile = "./assets/gifs/2ouverture_bloc.gif";
         break;
     }
   }
 
   validationStep() {
     if (this.stepView == 0) {
-      if (this.platform.is('core')) {
+      if (this.platform.is("core")) {
         // Running on desktop
         // Skipping CameraPage as it requires Cordova, unavailable on regular browser
         this.navCtrl.push(DefiningLayerPage, {
           stepView: this.stepView + 2
-        })
-
+        });
       } else {
         this.navCtrl.push(CameraPage, {
           stepView: this.stepView + 1
-        })
+        });
       }
     } else {
       this.dataService.setCurrentLayer(0);
-      if (this.platform.is('core')) {
+      if (this.platform.is("core")) {
         // Running on desktop
         // Skipping CameraPage as it requires Cordova, unavailable on regular browser
-        this.navCtrl.push(Notation1Page)
+        this.navCtrl.push(Notation1Page);
       } else {
-        this.navCtrl.push(CameraPage, { stepView: 5 })
+        this.navCtrl.push(CameraPage, { stepView: 5 });
       }
     }
   }
-
 }
