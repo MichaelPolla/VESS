ce8e4d25ebe7f50b58a1e12ded18bf3c9f805699
Michaël Polla <michael@polla.net>
2018-08-24 10:45:44 +0200

    VSCode auto-formatting

--- app/src/pages/parcels-tests/parcels-tests.ts
+++ app/src/pages/parcels-tests/parcels-tests.ts
@@ -1,18 +1,23 @@
-import { ModalPicturePage } from './../modal-picture/modal-picture';
-import { UserType } from './../../models/user';
-import { Component } from '@angular/core';
-import { Storage } from '@ionic/storage';
-import { AlertController, NavController, NavParams, ModalController } from 'ionic-angular';
+import { ModalPicturePage } from "./../modal-picture/modal-picture";
+import { UserType } from "./../../models/user";
+import { Component } from "@angular/core";
+import { Storage } from "@ionic/storage";
+import {
+  AlertController,
+  NavController,
+  NavParams,
+  ModalController
+} from "ionic-angular";
 
-import { Parcel, Test } from '../../models/parcel';
-import { User } from '../../models/user';
+import { Parcel, Test } from "../../models/parcel";
+import { User } from "../../models/user";
 // Pages
-import { GifViewPage } from '../gif-view/gif-view';
+import { GifViewPage } from "../gif-view/gif-view";
 // Providers
-import { DataService } from '../../providers/data-service';
-import { Toasts } from './../../providers/toasts';
-import { TranslateProvider } from '../../providers/translate/translate'
-import { Utils } from '../../providers/utils';
+import { DataService } from "../../providers/data-service";
+import { Toasts } from "./../../providers/toasts";
+import { TranslateProvider } from "../../providers/translate/translate";
+import { Utils } from "../../providers/utils";
 
 enum Steps {
   Parcels,
@@ -20,11 +25,10 @@ enum Steps {
 }
 
 @Component({
-  selector: 'page-parcels-tests',
-  templateUrl: 'parcels-tests.html',
+  selector: "page-parcels-tests",
+  templateUrl: "parcels-tests.html"
 })
 export class ParcelsTestsPage {
-
   public pageTitle: string;
   public listHeader: string;
   private stepNumber: number;
@@ -36,7 +40,8 @@ export class ParcelsTestsPage {
   public editMode: Boolean = false;
   private isConsultation: Boolean;
 
-  constructor(public navCtrl: NavController,
+  constructor(
+    public navCtrl: NavController,
     public alertCtrl: AlertController,
     public dataService: DataService,
     public modalCtrl: ModalController,
@@ -44,30 +49,34 @@ export class ParcelsTestsPage {
     public storage: Storage,
     private toasts: Toasts,
     private translate: TranslateProvider
-  ) { }
+  ) {}
 
   ionViewDidLoad() {
-    this.isConsultation = this.navParams.get('isConsultation');
-    this.stepNumber = this.navParams.get('step');
-    this.dataService.getParcels().then((value) => {
+    this.isConsultation = this.navParams.get("isConsultation");
+    this.stepNumber = this.navParams.get("step");
+    this.dataService.getParcels().then(value => {
       if (value != null) {
         this.parcels = value;
         this.currentParcel = this.dataService.getCurrentParcel();
       }
 
-
       switch (this.stepNumber) {
         case Steps.Tests:
-          this.pageTitle = this.translate.get('TESTS');
-          this.listHeader = this.translate.get('PARCEL_NAME', { name: this.currentParcel.name });
+          this.pageTitle = this.translate.get("TESTS");
+          this.listHeader = this.translate.get("PARCEL_NAME", {
+            name: this.currentParcel.name
+          });
 
           // Gets the completed tests if in Consultation mode, or the not completed ones if in Notation mode.
-          this.listItems = this.currentParcel.tests.filter(test => test.isCompleted === this.isConsultation);
+          this.listItems = this.currentParcel.tests.filter(
+            test => test.isCompleted === this.isConsultation
+          );
           break;
 
-        default: // Steps.Parcels, hopefully
+        default:
+          // Steps.Parcels, hopefully
           this.stepNumber = Steps.Parcels;
-          this.pageTitle = this.translate.get('PARCELS_TO_TEST');
+          this.pageTitle = this.translate.get("PARCELS_TO_TEST");
           this.listItems = this.parcels;
           break;
       }
@@ -76,19 +85,22 @@ export class ParcelsTestsPage {
 
     //Todo : should not create a new user here if non-existent
     if (!this.user) {
-      this.dataService.getUserInfo().then((value) => {
+      this.dataService.getUserInfo().then(value => {
         if (value != null) {
           this.user = value;
         } else {
-          this.user = new User({ firstName: "", lastName: "", userType: UserType.Anonymous, mail: "", idOfag: "", language: "" });
+          this.user = new User({
+            firstName: "",
+            lastName: "",
+            userType: UserType.Anonymous,
+            mail: "",
+            idOfag: "",
+            language: ""
+          });
         }
       });
     }
-
   }
-
-
-
 
   /**
    * Add a new item (Parcel or Test, depending on which step we are in).
@@ -97,54 +109,94 @@ export class ParcelsTestsPage {
     let inputsList: any;
     switch (this.stepNumber) {
       case Steps.Parcels:
-        if(this.user.userType==UserType.Ofag){
+        if (this.user.userType == UserType.Ofag) {
           inputsList = [
-            { name: 'name', placeholder: this.translate.get('NAME'), value: this.translate.get('PARCEL') + " " },
-            { name: 'ofag', placeholder: 'Identifiant OFAG', value: this.user.idOfag }
+            {
+              name: "name",
+              placeholder: this.translate.get("NAME"),
+              value: this.translate.get("PARCEL") + " "
+            },
+            {
+              name: "ofag",
+              placeholder: "Identifiant OFAG",
+              value: this.user.idOfag
+            }
           ];
-        }else{
+        } else {
           inputsList = [
-            { name: 'name', placeholder: this.translate.get('NAME'), value: this.translate.get('PARCEL') + " " }
+            {
+              name: "name",
+              placeholder: this.translate.get("NAME"),
+              value: this.translate.get("PARCEL") + " "
+            }
           ];
         }
-        
+
         break;
       case Steps.Tests:
         inputsList = [
-          { name: 'name', placeholder: this.translate.get('NAME'), value: 'Test ' },
-          { name: 'date', placeholder: this.translate.get('DATE'), value: Utils.getCurrentDatetime('dd/MM/y') }
+          {
+            name: "name",
+            placeholder: this.translate.get("NAME"),
+            value: "Test "
+          },
+          {
+            name: "date",
+            placeholder: this.translate.get("DATE"),
+            value: Utils.getCurrentDatetime("dd/MM/y")
+          }
         ];
         break;
-
     }
 
     let prompt = this.alertCtrl.create({
-      title: this.translate.get('ADD'),
+      title: this.translate.get("ADD"),
       inputs: inputsList,
       buttons: [
         {
-          text: this.translate.get('CANCEL'),
+          text: this.translate.get("CANCEL")
         },
         {
-          text: this.translate.get('VALIDATE'),
+          text: this.translate.get("VALIDATE"),
           handler: data => {
             switch (this.stepNumber) {
               case Steps.Parcels:
-                let parcelId = this.parcels.length > 0 ? this.parcels[this.parcels.length - 1].id + 1 : 1;
-                let parcel = new Parcel({ id: parcelId, name: data['name'], ofag: data['ofag'], tests: [] });
+                let parcelId =
+                  this.parcels.length > 0
+                    ? this.parcels[this.parcels.length - 1].id + 1
+                    : 1;
+                let parcel = new Parcel({
+                  id: parcelId,
+                  name: data["name"],
+                  ofag: data["ofag"],
+                  tests: []
+                });
                 this.parcels.push(parcel);
                 this.dataService.saveParcels();
                 break;
               case Steps.Tests:
-                let testId = this.currentParcel.tests.length > 0 ? this.currentParcel.tests[this.currentParcel.tests.length - 1].id + 1 : 1;
-                let test = new Test({ id: testId, name: data['name'], date: data['date'], layers: [] });
-                let parcelIndex = this.parcels.indexOf(this.dataService.getCurrentParcel());
+                let testId =
+                  this.currentParcel.tests.length > 0
+                    ? this.currentParcel.tests[
+                        this.currentParcel.tests.length - 1
+                      ].id + 1
+                    : 1;
+                let test = new Test({
+                  id: testId,
+                  name: data["name"],
+                  date: data["date"],
+                  layers: []
+                });
+                let parcelIndex = this.parcels.indexOf(
+                  this.dataService.getCurrentParcel()
+                );
                 this.parcels[parcelIndex].tests.push(test);
                 this.dataService.saveParcels();
-                this.listItems = this.currentParcel.tests.filter(test => test.isCompleted === this.isConsultation);
+                this.listItems = this.currentParcel.tests.filter(
+                  test => test.isCompleted === this.isConsultation
+                );
                 break;
             }
-            
           }
         }
       ]
@@ -152,14 +204,15 @@ export class ParcelsTestsPage {
     prompt.present();
   }
 
-
   protected editItem(item: Parcel | Test) {
     // TODO
     // Basically the same as AddItem, except we need to get the item selected
     // and show its actual values in the formular.
-    // See also the manageItem method ; it wasn't complete but shows a way to 
+    // See also the manageItem method ; it wasn't complete but shows a way to
     // get the selected item.
-    this.toasts.showToast(this.translate.get('FUNCTIONALITY_NOT_YET_AVAILABLE'));
+    this.toasts.showToast(
+      this.translate.get("FUNCTIONALITY_NOT_YET_AVAILABLE")
+    );
   }
 
   /**
@@ -227,7 +280,7 @@ export class ParcelsTestsPage {
   //   }
   // }
 
-  protected deleteItem(event,item: Test | Parcel) {
+  protected deleteItem(event, item: Test | Parcel) {
     event.stopPropagation();
     switch (this.stepNumber) {
       case Steps.Parcels:
@@ -235,7 +288,9 @@ export class ParcelsTestsPage {
         break;
       case Steps.Tests:
         this.dataService.deleteTest(item as Test);
-        this.listItems = this.currentParcel.tests.filter(test => test.isCompleted === this.isConsultation);
+        this.listItems = this.currentParcel.tests.filter(
+          test => test.isCompleted === this.isConsultation
+        );
         break;
     }
     this.dataService.saveParcels();
@@ -245,7 +300,10 @@ export class ParcelsTestsPage {
     switch (this.stepNumber) {
       case Steps.Parcels:
         this.dataService.setCurrentParcel((item as Parcel).id);
-        this.navCtrl.push(ParcelsTestsPage, { isConsultation: this.isConsultation, step: this.stepNumber + 1 });
+        this.navCtrl.push(ParcelsTestsPage, {
+          isConsultation: this.isConsultation,
+          step: this.stepNumber + 1
+        });
         break;
       case Steps.Tests:
         if (this.isConsultation) {
@@ -253,14 +311,15 @@ export class ParcelsTestsPage {
         } else {
           this.dataService.setCurrentTest((item as Test).id);
           this.navCtrl.push(GifViewPage);
-          break
+          break;
         }
-
     }
   }
 
   private showSummary(test: Test) {
-    this.modalCtrl.create(ModalPicturePage, {type: "resume", resume: test}).present();
+    this.modalCtrl
+      .create(ModalPicturePage, { type: "resume", resume: test })
+      .present();
   }
 
   protected switchEditMode() {
@@ -269,6 +328,5 @@ export class ParcelsTestsPage {
     } else {
       this.editMode = true;
     }
-
   }
 }
